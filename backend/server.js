f90fc8d0190bb9cb55ff1914d575a1069b540dd1
Aanchal Aggarwal <aanchalaggarwal0805@gmail.com>
2026-02-20 01:27:59 +0530

    prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,11 @@ app.use(express.json());
 // Use the auth routes
 app.use("/auth", authRoutes);
 
+// const PORT = process.env.PORT || 5000;
+// app.listen(5000, () => console.log(`Server running on port ${PORT}`));
 const PORT = process.env.PORT || 5000;
-app.listen(5000, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
