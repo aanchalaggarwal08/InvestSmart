f504a790bdbdeec39d25b7b6135a39f1b7c9819b
Aanchal Aggarwal <aanchalaggarwal0805@gmail.com>
2026-02-21 01:58:55 +0530

    fix navbar filename case

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.js";
 
 import Home from "./pages/home";
 import Login from "./pages/Login";
