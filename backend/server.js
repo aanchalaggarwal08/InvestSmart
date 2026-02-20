6b88be5b1d3980215addf4c0fadc1add7fc93ef7
Aanchal Aggarwal <aanchalaggarwal0805@gmail.com>
2026-02-21 00:55:10 +0530

    fix netlify routing

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,4 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/auth.js";
-// import mongoose from "mongoose";
 
-
-// dotenv.config();
-// mongoose.connect(process.env.MONGO_URI)
-// .then(() => console.log("MongoDB connected"))
-// .catch(err => console.log("MongoDB error:", err));
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// // Use the auth routes
-// app.use("/auth", authRoutes);
-
-// // const PORT = process.env.PORT || 5000;
-// // app.listen(5000, () => console.log(`Server running on port ${PORT}`));
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 
 import express from "express";
 import cors from "cors";
@@ -38,7 +13,13 @@ const app = express();
 /* ===============================
    MIDDLEWARE
 ================================ */
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://investsmart-app.netlify.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 /* ===============================
