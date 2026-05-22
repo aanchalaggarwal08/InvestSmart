f5d17aecae3257c34d0769138f0739850538527c
Aanchal Aggarwal <aanchalaggarwal0805@gmail.com>
2026-05-22 18:06:06 +0530

    fix login css casing

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -1,6 +1,6 @@
 
 
-import "./Login.css";
+import "./login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "../context/authContext";
